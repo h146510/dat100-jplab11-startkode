8461cc9afca3b4c7fbee3470c2cc9256a5cb5a07
Jarle <Jarle@158.37.235.155>
2021-11-05 14:47:08 +0100

    234

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave1.*;
+import no.hvl.dat100.jplab11.oppgave2.Bilde;
+import no.hvl.dat100.jplab11.oppgave2.Tekst;
 
 public class Blogg {
 	
 	private int lengde;
-	private int antall;
 	private int id;
 	private int dato;
 	private String tekst;
+	private String bruker;
 	private String url;
 	private Innlegg[] innleggtabell;
+	private int nesteledige;
 
 	// TODO: objektvariable 
 
@@ -28,7 +31,7 @@ public class Blogg {
 	}
 
 	public int getAntall() {
-		return this.antall;
+		return this.nesteledige;
 	}
 	
 	public Innlegg[] getSamling() {
@@ -36,13 +39,27 @@ public class Blogg {
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		boolean funnet = false;
+		int pos = 0;
+		while (pos < nesteledige && !funnet) {
+			if (innleggtabell[pos] == innlegg)
+			funnet = true;
+			else 
+			pos++;
+		}
+		if (funnet) return pos;
+		else 
+			return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		return false;
-}
+		int pos = finnInnlegg(innlegg);
+		if (pos >= 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	public boolean ledigPlass() { 
 		  for (int i = 0; i < innleggtabell.length; i++) {
@@ -55,21 +72,31 @@ public class Blogg {
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-		 {
-			
+		boolean ny = finnInnlegg(innlegg) == -1;
+		if(ny&& nesteledige < innleggtabell.length) {
+			innleggtabell[nesteledige] = innlegg;
+			nesteledige ++;
+			return true;
+		} else {
+			return false;
 		}
-		return true;
-	}
+}
+	
 	
 	public String toString() {
+		
 //		"2\n" + 
 //				"TEKST\n1\nOle Olsen\n23-10\n0\nen tekst\n" + 
 //				"BILDE\n2\nOline Olsen\n24-10\n0\net bilde\nhttp://www.picture.com/oo.jpg\n";
-		return "2\n" +
 		
-		 "TEKST" + "\n" + id + "\n" + dato + "\n" + tekst + "\n" +
-		
-		 "BILDE" + "\n" + id + "\n" + dato + "\n" + tekst + "\n" + url + "\n";
+//	Blogg samling = new Blogg(2);
+//		
+//		Tekst innlegg1 = new Tekst(1,"Ole Olsen","23-10","en tekst");
+//		Bilde innlegg2 = new Bilde(2,"Oline Olsen","24-10","et bilde","http://www.picture.com/oo.jpg");
+//		
+//		2\nTEKST\n1\nOle Olsen\n23-10\n0\nen tekst\nBILDE\n2\nOline Olsen\n24-10\n0\net bilde\nhttp://www.picture.com/oo.jpg\n
+//
+		return"2\n" + "TEKST" + "\n" + id + "\n" + bruker + "\n" + dato + "\n" + tekst + "\n" + "BILDE" + "\n" + id + "\n" + bruker + "\n" + dato + "\n" + tekst + "\n" + url + "\n";
 	}
 
 	// valgfrie oppgaver nedenfor
