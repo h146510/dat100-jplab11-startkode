bbc85362970f2123249241bbd4fa51a198ee08fc
h600862 <600862@stud.hvl.no>
2021-11-08 14:56:41 +0100

    Oppgave 4

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,38 +1,24 @@
-//package no.hvl.dat100.jplab11.oppgave4;
-//
-//import java.io.FileNotFoundException;
-//import java.io.PrintWriter;
-//import java.io.FileWriter;
-//import java.io.File;
-//
-//static String MAPPE_STR = System.getProperty("user.dir") + "Users/git/dat100-jplab11-testing/src/no/hvl/dat100/tests/bloggkorrect.dat";
-//
-//import no.hvl.dat100.jplab11.common.TODO;
-//import no.hvl.dat100.jplab11.oppgave3.*;
-//
-//public class SkrivBlogg {
-//
-//	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-//
-////		String filnavn;
-////
-////		  filnavn = MAPPE_STR;
-////
-////		  File file = new File(MAPPE_STR + filnavn);
-////		  Scanner reader = new Scanner(file);
-//
-//		  int linenumber = 1;
-//
-//		  // les innhold i filen linje for linje
-//		  String line;
-//
-//		  while (reader.hasNextLine()) {
-//		  line = reader.nextLine();
-//		  System.out.println(linenumber + " " + line);
-//		  linenumber++;
-//		  }
-//
-//		  reader.close();
-//		
+package no.hvl.dat100.jplab11.oppgave4;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+
+import no.hvl.dat100.jplab11.common.TODO;
+import no.hvl.dat100.jplab11.oppgave3.Blogg;
+
+public class SkrivBlogg {
+
+	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+		
+		try {
+			File fil = new File(mappe+filnavn);
+			PrintWriter skriver = new PrintWriter(fil);
+			skriver.print(samling.toString());
+			skriver.close();
+			return true;
+		} catch (FileNotFoundException e) {
+			return false;
+		}
 	}
 }
